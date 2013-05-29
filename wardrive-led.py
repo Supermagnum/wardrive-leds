0fb55056e0e39fecc360c24522f12d520259d096
Supermagnum <krbjhvee@online.no>
2013-05-29 18:18:13 +0200

    Update wardrive-led.py

--- wardrive-led.py
+++ wardrive-led.py
@@ -1,4 +1,5 @@
-#This script controlls a LED connected to the GPIO header,and lights it up if gpsd is running.
+#This script controlls a LED connected to the GPIO header,and lights it up if gpsd and kismet is running.
+#kismet must be started automatic on boot with the command kismet_server --daemonize
 import RPi.GPIO as GPIO
 import os
 import subprocess
@@ -16,8 +17,14 @@ while 1 == 1:
         if 'gpsd -F /var/run/gpsd.sock' in output: GPIO.output(17, True) #Turn on GPSD LED
         else:
                 GPIO.output(17, False) #Turn off GPSD LED
-                
-#todo:  implement automatic upload of logfiles to wigle.net,if a switch is toggled, password and username set in script or a separate file.
+while 1 == 1:
+        kismet = subprocess.Popen(['ps -ef | grep gpsd'],
+        stdout = subprocess.PIPE, shell=True) #Assigns the output from the grep to the kismet variable
+        (output, error) =kismet.communicate()
+        if 'kismet_server --daemonize' in output: GPIO.output(04,True)
+        else:
+                GPIO.output(04, False)
+#todo: implement automatic upload of logfiles to wigle.net,if a switch is toggled, password and username set in script or a separate file.
 #implement GPS locked LED
 #wigle.net upload in progress LED
 #Please feel free to contribute to this script
