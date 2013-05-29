fe8728ddac7f2f83e40f9d0f5154adce02297ec6
Håken <haaken@haken-cooler-master-690.(none)>
2013-05-29 02:23:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+wardrive-led-controll
+This is a Python script that controlls the leds connected to  the GPIO header on a
+Rasberry Pi model B.
+It checks if kismet_server and gpsd is running,and controlls LED1 and LED2 accordingly.
+
+Licence: 
+Creative Commons Attribution-NonCommercial 3.0 Unported.
---
+++ wardrive-leds.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import os
+import subprocess
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(4, GPIO.OUT)#LED KISMET.
+GPIO.setup(17, GPIO.OUT)#LED GPSD running.
+#Makes sure all LED's are at the same state initially (off):
+GPIO.output(4, True)
+GPIO.output(17, True)
+while 1 < 2:
+        kismet = subprocess.Popen(['ps -ef | grep kismet_server'])#checks if kismet_server is running.
+        stdout = subprocess.PIPE, shell=True() #Assigns the output from the grep to the kismet variable
+        (output, error) = kismet.communicate()
+        if 'kismet_server' in output: GPIO.output(4, False) #Turn on KISMET LED
+else:
+        GPIO.output(4, True) #Turn off KISMET LED
+        gps = subprocess.Popen(['ps -ef | grep gpsd'],
+        stdout = subprocess.PIPE, shell=True)#Assigns the output from the grep to the gps variable
+        (output, error) = gps.communicate()
+        if 'gpsd' in output: GPIO.output(17, False) #Turn on GPSD LED
+        else:
+              GPIO.output(17, True) #Turn off GPSD LED 
